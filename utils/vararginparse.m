9adaadea6a2a827f28c9624d46d4ed7f205b3365
Ali Tafakkor <atafakko@uwo.ca>
2023-03-27 11:50:40 -0400

    adding toolbox utils (vararginparse that supports required and optional variables to parse)

---
+++ utils/vararginparse.m
@@ -0,0 +1,23 @@
+function vararginparse(cellarray, reqvarstoparse, opvarstoparse)
+    if(~isempty(cellarray))
+        inargnames = cellarray{1:2:end};
+    else
+        inargnames = {};
+    end
+
+    for i = 1:length(reqvarstoparse)
+        if any(strcmp(inargnames, reqvarstoparse{i}))
+            ind = find(strcmp(inargnames, reqvarstoparse{i}));
+            assignin('caller', cellarray{2*ind-1}, cellarray{2*ind});
+        else
+            error(sprintf("%s is required but was'nt found", reqvarstoparse{i}))
+        end
+    end
+
+    for i = 1:length(opvarstoparse)
+        if any(strcmp(inargnames, opvarstoparse{i}))
+            ind = find(strcmp(inargnames, opvarstoparse{i}));
+            assignin('caller', cellarray{2*ind-1}, cellarray{2*ind});
+        end
+    end
+end
