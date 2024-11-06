d15259563719b56ed646c2459d847e384408b3b3
Ali <alitafakkor@gmail.com>
2024-11-06 18:02:15 -0500

    temporarily removing models directory to reformatting

--- Models/spectrotemporal-synthesis-v2/assert_equal_dimensions.m
+++
@@ -1,9 +0,0 @@
-function assert_equal_dimensions(X,Y)
-
-% tests whether too matrices have the same size
-% 
-% assert_equal_dimensions(rand(4,3), rand(4,3))
-% assert_equal_dimensions(rand(4,3), rand(4,2))
-
-Z = size(X) == size(Y);
-assert(all(Z(:)));
--- Models/spectrotemporal-synthesis-v2/unwrap_allbutlast.m
+++
@@ -1,7 +0,0 @@
-function X = unwrap_allbutlast(X)
-
-% unwrap all of the dimensions but the last in array X
-
-dims = size(X);
-X = reshape(X, [prod(dims(1:end-1)), dims(end)]);
-
