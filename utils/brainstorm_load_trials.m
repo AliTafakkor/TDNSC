bef6fd895ada7a1df0436e7e5ef328644bdddfda
Ali <alitafakkor@gmail.com>
2024-11-08 16:10:19 -0500

    added utils function for loading brainstorm trials

---
+++ utils/brainstorm_load_trials.m
@@ -0,0 +1,100 @@
+function [trials, time] = brainstorm_load_trials(trialsdir, condition, channelname)
+% BRAINSTORM_LOAD_TRIALS Loads all trials for a specified subject and condition, 
+% excludes bad trials, and loads data for the specified channels only.
+%
+% INPUTS:
+%   trialsdir   - String specifying the path to the directory containing the trial .mat files.
+%   condition   - Integer specifying the condition for which to load the trials.
+%   channelname - Cell array of strings specifying the names of channels to load,
+%                 e.g., {'MEG', 'EEG', 'A', 'B'}.
+%
+% OUTPUTS:
+%   trials      - 3D array (channels x time x trials) containing the loaded trials data after excluding bad trials.
+%   time        - Vector specifying the time points associated with the trials.
+%
+% EXAMPLE USAGE:
+%   [trials, time] = brainstorm_load_trials('path/to/trials', 1, {'MEG', 'EEG'});
+%
+% NOTES:
+%   - This function requires a brainstormstudy.mat file in the trials directory 
+%     and a channel.mat file either in the same folder or under @default_study in the subject folder.
+%   - It automatically excludes bad trials from the loaded data.
+%   - Only the specified channels are imported for analysis.
+%
+%   Ali Tafakkor (atafakko@uwo.ca), University of Western Ontario
+
+% Get subject directory
+[subjdir, ~, ~] = fileparts(trialsdir);
+
+% Find channel file
+try 
+    channelfile = dir(fullfile(trialsdir,'channel*.mat'));
+    Channel = load(fullfile(channelfile.folder,channelfile.name));
+    disp('subject-specific channel found!');
+catch
+    channelfile = dir(fullfile(subjdir,'@default_study','channel*.mat'));
+    Channel = load(fullfile(subjdir,'@default_study',channelfile.name));
+end 
+Channel = Channel.Channel;
+
+% Find channelnames
+for ch = 1:length(channelname)
+    pos = strfind({Channel.Name},channelname{ch});
+    ndx{ch} = find(~cellfun(@isempty,pos));
+end
+channelndx = unique([ndx{:}],'stable');
+
+% Find trial files
+ftrials = dir(fullfile(trialsdir,sprintf('*data_%d_trial*',condition)));
+
+% Remove bad trials from trials found
+bststudy = load(fullfile(trialsdir,'brainstormstudy.mat'));
+ndx = [];
+for i = 1:length(bststudy.BadTrials)
+    pos = strfind({ftrials.name},bststudy.BadTrials{i});
+    bad_ind = find(~cellfun(@isempty,pos));
+    if ~isempty(bad_ind)
+        ndx = [ndx, bad_ind];
+    end
+end
+ftrials(ndx) = [];
+
+% Read trials
+for i = 1:length(ftrials)
+    data = load(fullfile(trialsdir,ftrials(i).name));
+    trials{i} = data.F(channelndx,:);
+    time = data.Time;
+
+    % get import time from history
+    [nrow,ncol] = size(data.History);
+    for j = 1:nrow
+        if strcmp(data.History(j,2),'import_time')
+            t = sscanf(data.History{j,3}(6:end),['%f']);
+            trial_import_time(i) = t;
+        end
+    end
+    
+end
+
+% Reorder trials based on import time
+try
+[~,I] = sort(trial_import_time);
+trial_import_time = trial_import_time(I);
+trials = trials(I);
+end
+
+% Convert to matrix
+trials = single(cat(3,trials{:}));
+
+
+
+
+
+
+
+
+
+
+
+
+
